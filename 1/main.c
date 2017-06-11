a6cabaf400530d218429f5455c67d1f6de9814a9
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-06-11 14:50:24 +0700

    .

--- 1/main.c
+++ 1/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/x509.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 
 void block_(char*string,unsigned char*result,unsigned char*result1,unsigned long * counter)
 {
@@ -19,7 +24,6 @@ void block_(char*string,unsigned char*result,unsigned char*result1,unsigned long
 
 void first(char*string)
 {
-
  unsigned char result __attribute__((aligned(8)));
  unsigned char result1 __attribute__((aligned(8)));
  unsigned long counter_string_size=0;
@@ -33,7 +37,11 @@ for(counter_string_size;counter_string_size--;)
 
 void second(char*string)
 {
-
+char hash[SHA512_DIGEST_LENGTH];
+SHA512(string, strlen(string) - 1, hash);
+for(unsigned short i =0;hash[i];i++)
+ *string++=hash[i++];
+*string++='\0';
 }
 
 void final(char*string)
@@ -45,5 +53,6 @@ void main(void)
 {
 unsigned char * buffer = strdup("Test String");
 first(buffer);
-printf("Result: %d\n",*buffer);
+second(buffer);
+printf("Result: %s\n",buffer);
 }
